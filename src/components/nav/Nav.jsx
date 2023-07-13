3f47abc0820cfaef86069c0aa02f8478f240642b
itsrayito <rayito.aguirre94@gmail.com>
2023-07-13 01:27:22 -0500

    react icons for nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './nav.css'
+import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
 
 function Nav() {
     return (
-        <div>Nav</div>
+        <><div>Nav</div><nav>
+            <a href="#"><BiHomeAlt /></a>
+            <a href="#about"><BiUser /></a>
+            <a href="#experience"><BiBook /></a>
+            <a href="#services"><RiServiceLine /></a>
+            <a href="#contact"><BiMessageSquareDetail /></a>
+        </nav></>
     )
 }
 
