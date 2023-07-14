ed2fc86b97798248e982e9ce0707b85c4287514b
itsrayito <rayito.aguirre94@gmail.com>
2023-07-14 03:25:37 -0500

    addition of the services jsx & change of icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './nav.css';
-import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail, BiStar} from 'react-icons/bi'
+import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail, BiCodeCurly} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 
 function Nav() {
@@ -9,7 +9,7 @@ function Nav() {
         <nav>
             <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
             <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
-            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiStar/></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeCurly/></a>
             <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
             <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiBook/></a>
             <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import './services.css'
+import {FiCheck} from 'react-icons/fi'
 
 function Services() {
     return (
-        <section id='services'>Services</section>
+        <section id='services'>
+            <h5>Let's do this</h5>
+            <h2>Services</h2>
+
+            <div className="container services__container">
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>UX/UI Design</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* section for service 2 */}
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>service 2</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* ----- section for service 3 */}
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>service 2</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                        <li><FiCheck className='service__list-icon'/>
+                        <p>Elementum taciti nec curabitur tempor bibendum eros</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
     )
 }
 
