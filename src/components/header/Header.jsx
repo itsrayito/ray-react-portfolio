ef3693c819c255e5bcfb1042c31b98a0289d86b4
itsrayito <rayito.aguirre94@gmail.com>
2023-07-15 15:23:39 -0500

    minor debugging

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import Me from '../../assets/rayito.png'
-import HeaderSocials from './HeaderSocials';
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import Me from "../../assets/rayito.png";
+import HeaderSocials from "./HeaderSocials";
 
 function Header() {
     return (
-        <header>
-            <div className="container header__container">
-                <h5> Hey Ya'll, my name is</h5>
-                <h1>Rayito Aguirre</h1>
-                <h5 className='text-dark'>Full-Stack Developer</h5>
-                <CTA />
-                <HeaderSocials />
-                <div className="me">
-                    <img src={Me} alt='Rayito Aguirre'/>
-                </div>
-
-                <a href="#header" className='scroll__up'> Scroll Down </a>
+       <>
+       <header id="header">
+        <div className="container header__container">
+            <h5>Hey Ya'll</h5>
+            <h1>I am Rayito</h1>
+            <h5 className="text-light">Full-Stack Developer</h5>
+            <CTA />
+        <div>
+        <div className="me">
+            <img src={Me} alt="image of Rayito Aguirre" />
             </div>
+        </div>
+    </div>
         </header>
-    )
+        <HeaderSocials />
+        <a href="#footer" className="scroll__down">
+            {" "}
+            Scroll Down{" "}
+        </a>
+        </>
+    );
 }
 
-export default Header
+export default Header;
