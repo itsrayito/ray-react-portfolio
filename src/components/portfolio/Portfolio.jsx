15596e9e62b1eb309c544dafffe7e5b02fc013ac
itsrayito <rayito.aguirre94@gmail.com>
2023-07-15 06:01:06 -0500

    adjusting portfolio section with uploads

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,55 +1,93 @@
-import React from 'react';
-import './portfolio.css'
-import cover1 from '../../assets/note-taker.png'
-import cover2 from '../../assets/oasis.png'
-import cover3 from '../../assets/text-editor.png'
-import cover4 from '../../assets/marvel.png'
+import React from "react";
+import "./portfolio.css";
+import cover1 from "../../assets/note-taker.png";
+import cover2 from "../../assets/oasis.png";
+import cover3 from "../../assets/text-editor.png";
+import cover4 from "../../assets/marvel.png";
+import cover5 from "../../assets/empty scheduler.png";
+import cover6 from "../../assets/weather.png";
+
+const data = [
+    {
+        id: 1,
+        image: cover1,
+        title: "Note-Taker",
+        github: "https://github.com/itsrayito/Express.js-Note-Taker",
+        demo: "https://note-taker-ra.herokuapp.com/",
+    },
+    {
+        id: 2,
+        image: cover2,
+        title: "Oasis",
+        github: "https://github.com/robobrownie13/animal-blog-group-4-p2",
+        demo: "https://salty-badlands-43491.herokuapp.com/",
+    },
+    {
+        id: 3,
+        image: cover3,
+        title: "Text-Editor",
+        github: "https://github.com/itsrayito/PWA-text-editor",
+        demo: "https://lovely-jate-2fa379ab8708.herokuapp.com/",
+    },
+    {
+        id: 4,
+        image: cover4,
+        title: "Marvel Gifs Assemble",
+        github: "https://github.com/jdel-18/first-group-project",
+        demo: "https://jdel-18.github.io/first-group-project/",
+    },
+    {
+        id: 5,
+        image: cover5,
+        title: "Work-Day-Scheduler",
+        github: "https://github.com/itsrayito/Work-Day-Scheduler",
+        demo: "https://itsrayito.github.io/Work-Day-Scheduler/",
+    },
+    {
+        id: 6,
+        image: cover6,
+        title: "Weather-Dashboard",
+        github: "https://github.com/itsrayito/Weather-Dashboard",
+        demo: "https://itsrayito.github.io/Weather-Dashboard/",
+    },
+];
 
 function Portfolio() {
     return (
-            <section id='portfolio'>
-                <h5>My Works</h5>
-                <h2>Portfolio</h2>
+        <section id="portfolio">
+            <h5>My works</h5>
+            <h2>Portfolio</h2>
 
-                <div className="container portfolio__container">
-                    <article className='portfolio__item'>
-                        <div className='portfolio__item'>
-                            <img src={cover1} alt="Project 1" />
-                        </div>
-                        <h3>Note Taker</h3>
-                        <a href="https://github.com/itsrayito" className='btn' target='_blank'>GitHub Repository</a>
-                        <a href="https://github.com/itsrayito" className='btn btn-primary' target='_blank'>Live Site</a>
-                    </article>
-
-                    <article className='portfolio__item'>
-                        <div className='portfolio__item'>
-                            <img src={cover2} alt="Project 1" />
-                        </div>
-                        <h3>Oasis</h3>
-                        <a href="https://github.com/itsrayito" className='btn' target='_blank'>GitHub Repository</a>
-                        <a href="https://gothub.com/itsrayito" className='btn btn-primary' target='_blank'>Live Site</a>
-                    </article>
-
-                    <article className='portfolio__item'>
-                        <div className='portfolio__item'>
-                            <img src={cover3} alt="Project 1" />
-                        </div>
-                        <h3>Text Editor</h3>
-                        <a href="https://github.com/itsrayito" className='btn' target='_blank'>GitHub Repository</a>
-                        <a href="https://gothub.com/itsrayito" className='btn btn-primary' target='_blank'>Live Site</a>
-                    </article>
-
-                    <article className='portfolio__item'>
-                        <div className='portfolio__item'>
-                            <img src={cover4} alt="Project 1" />
-                        </div>
-                        <h3>Marvel Gifs Assemble</h3>
-                        <a href="https://github.com/itsrayito" className='btn' target='_blank'>GitHub Repository</a>
-                        <a href="https://gothub.com/itsrayito" className='btn btn-primary' target='_blank'>Live Site</a>
-                    </article>
-                </div>
-            </section>
-    )
+            <div className="container portfolio__container">
+                {data.map(({ id, image, title, github, demo}) => {
+                    return (
+                        <article key={id} className="portfolio__item">
+                            <div className="portfolio__item-image">
+                                <img src={image} alt={title} />
+                            </div>
+                            <h3>{title}</h3>
+                            <div className="portfolio__item-cta">
+                                <a
+                                href={github}
+                                classname="btn"
+                                target="_blank"
+                                >
+                                    GitHub Repository
+                                    </a>
+                                    <a
+                                    href={demo}
+                                    classname="btn btn-primary"
+                                    target="_blank"
+                                    >
+                                    Live Demo
+                                    </a>
+                            </div>
+                        </article>
+                    );
+                })}
+            </div>
+        </section>
+    );
 }
 
-export default Portfolio
+export default Portfolio;
